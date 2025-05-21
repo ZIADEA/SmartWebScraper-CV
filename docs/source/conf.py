23fd41940db4d6e9fed952a11de2a3dc029c0f96
djeryala@gmail/com <143638424+ZIADEA@users.noreply.github.com>
2025-05-21 10:54:20 +0100

    back

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,17 +24,5 @@ language = 'fr'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_book_theme"
-
-html_theme_options = {
-    "repository_url": "https://github.com/ZIADEA/SmartWebScraper-CV",
-    "use_repository_button": True,
-    "use_issues_button": True,
-    "use_edit_page_button": False,
-    "show_navbar_depth": 2,
-    "path_to_docs": "docs/source",
-}
-
-html_title = "SmartWebScraper-CV"
-
-
+html_theme = 'alabaster'
+html_static_path = ['_static']
