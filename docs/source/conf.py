15564fae69b11f05c7553a03fc9c598adfe12394
ZIDEA <djeryala@gmail.com>
2025-06-16 18:03:58 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,14 +7,26 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'SmartWebScraper-CV'
-copyright = '2025, DJERI-ALASSANI OUBENOUPOU'
-author = 'DJERI-ALASSANI OUBENOUPOU'
-release = '1.0'
+copyright = '2025, DJERI-ALASSANI OUBENOUPOU & EL MAJDI WALID'
+author = 'DJERI-ALASSANI OUBENOUPOU & EL MAJDI WALID'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.imgmath',
+    'sphinx_rtd_theme',
+    'sphinx_copybutton',
+    'myst_parser',
+    'sphinxcontrib.mermaid'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +36,80 @@ language = 'fr'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': '#2980B9',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
+# Custom CSS
+html_css_files = [
+    'custom.css',
+]
+
+# Logo
+html_logo = '_static/logo.png'
+html_favicon = '_static/favicon.ico'
+
+# LaTeX output options
+latex_elements = {
+    'papersize': 'a4paper',
+    'pointsize': '12pt',
+    'preamble': r'''
+\usepackage[utf8]{inputenc}
+\usepackage[french]{babel}
+\usepackage{amsmath}
+\usepackage{amsfonts}
+\usepackage{amssymb}
+''',
+}
+
+latex_documents = [
+    ('index', 'SmartWebScraper-CV.tex', 'SmartWebScraper-CV Documentation',
+     'DJERI-ALASSANI OUBENOUPOU \\& EL MAJDI WALID', 'manual'),
+]
+
+# Intersphinx mapping
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+
+# Todo extension
+todo_include_todos = True
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+# MyST parser settings
+myst_enable_extensions = [
+    "deflist",
+    "tasklist",
+    "html_admonition",
+    "html_image",
+]
+
+# Copy button settings
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
